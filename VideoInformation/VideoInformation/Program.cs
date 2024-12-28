ebba8f88db03dd4bace9fb89c613ee22ddd3c11a
Kizuna Akari <lecion20@outlook.com>
2024-12-28 21:03:55 +0800

    simple mutex control make it queue up

--- VideoInformation/VideoInformation/Program.cs
+++ VideoInformation/VideoInformation/Program.cs
@@ -2,43 +2,56 @@ using VideoInformation.DBContexts;
 using VideoInformation.Services;
 using VideoInformation.Utilities;
 
-namespace VideoInformation
+namespace VideoInformation;
+
+public class Program
 {
-    public class Program
+    public static Mutex mutex = new(false);
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        // spin up
+        Env.DATABASEURL.ToString();
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<VideoContext>();
+        builder.Services.AddScoped<VideoService>();
+        builder.Services.AddScoped<CommentService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            // spin up
-            Env.DATABASEURL.ToString();
-
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<VideoContext>();
-            builder.Services.AddScoped<VideoService>();
-            builder.Services.AddScoped<CommentService>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            // app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        // app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.Use((context, next) =>
+        {
+            try
+            {
+                mutex.WaitOne();
+                return next(context);
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        });
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
